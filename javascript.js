058b7aebcd27f588515c0301641eaa9efe8beadc
Pedro-Senatus <pedrofigo123@gmail.com>
2023-07-17 16:10:56 -0300

    javascript

---
+++ javascript.js
@@ -0,0 +1,24 @@
+function rolarZelda() {
+
+    window.location.href = 'triple-a.html#metadeZ';
+    var targetElement = document.getElementById('metade');
+    var targetPosition = targetElement.offsetTop;
+    window.scrollTo(0, targetPosition);
+
+}
+
+function rolarStarDewValley() {
+
+    window.location.href = 'indies.html#metadeSDV';
+    var targetElement = document.getElementById('#metadeSDV');
+    var targetPosition = targetElement.offsetTop;
+    window.scrollTo(0, targetPosition);
+
+}
+
+function rolarLOL(){
+    window.location.href = 'competitivos.html#metadeLOL';
+    var targetElement = document.getElementById('#metadeLOL');
+    var targetPosition = targetElement.offsetTop;
+    window.scrollTo(0, targetPosition);
+}
